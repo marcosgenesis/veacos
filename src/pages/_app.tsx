16f268ec0149d3e23ba06580fd5295a0d7673c1c
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2023-02-25 20:35:49 -0300

    feat: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,9 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
-export { reportWebVitals } from 'next-axiom';
-import { QueryClient, QueryClientProvider, } from "@tanstack/react-query";
+export { reportWebVitals } from "next-axiom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
+        <Analytics />
       </QueryClientProvider>
     </SessionProvider>
   );
